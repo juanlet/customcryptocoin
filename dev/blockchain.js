acc3e836399362a9fbdf5b6b623f4b00ae13a72f
Juan Letamendia <juan.letamendia@fundkers.com>
2018-06-02 04:02:31 -0300

    blockchain methods finished

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,12 @@
+const sha256 = require('sha256');
+  
 function Blockchain(){
     //this contains blocks of transactions, these are set in stone once the blocks are pushed into this array
     this.chain = [];
     //pending transactions, this newTransactions are not set in stone until a new block is created/mined
     this.pendingTransactions = [];
+    //I create the genesys block, the first block of the chain
+    this.createNewBlock(100,'0','0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
@@ -10,7 +14,7 @@ Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        trassactions: this.pendingTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
@@ -29,7 +33,7 @@ Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length - 1];
 }
 
-Blockchain.prototype.createNewTransaction = function(amount, sender, recepient){
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
      const newTransaction = {
          amount: amount,
          sender: sender,
@@ -40,6 +44,30 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recepient){
 
      //return number of the block that this transaction will be added to
      return this.getLastBlock()['index'] + 1;
+
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash,currentBlockData,nonce){
+    //it will return a fixed length string(SHA256)
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    
+    const hash = sha256(dataAsString);
+    
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData){
+    //will generate hash until the hash starts with 4 zeros, it will probably have to do hundreds/thousands of iterations
+    //this nonce will change for each iteration to generate a new hash until the hash starts with 0000
+    let nonce = 0;
+
+    let hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+    while(hash.substring(0,4) !== '0000'){
+        nonce++;
+        hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+    }   
+    
+    return nonce;
 
 }
 
--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389,"3232DSADASDAS","DSADASDASD32");
-bitcoin.createNewBlock(321,"22222DASDAS","NAADASDASD32");
-bitcoin.createNewBlock(2877,"321452DSADASDAS","D44444SDASD32");
-
 console.log(bitcoin);
